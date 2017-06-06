2c2a05f24cbcdb854c2b31eb7c44fe11d6b0b6c2
dvoronov <voronov-d88@yandex.ru>
2017-06-06 08:23:12 +0300

    Add fetch_afisha_page and parse_afisha_list

--- cinemas.py
+++ cinemas.py
@@ -1,11 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 
 
 def fetch_afisha_page():
-    pass
+    request = requests.get('https://www.afisha.ru/msk/schedule_cinema/')
+    if request.status_code == requests.codes.ok:
+        return request.content
 
 
 def parse_afisha_list(raw_html):
-    pass
+    afisha_html_tree = BeautifulSoup(raw_html, 'html.parser')
+    film_elements = afisha_html_tree.find_all('div', {'class': 'object s-votes-hover-area collapsed'})
+    return [{'title': f.find('h3').text, 'count_cinema': len(f.find_all('tr'))} for f in film_elements]
 
 
 def fetch_movie_info(movie_title):
@@ -17,4 +23,5 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    html = fetch_afisha_page()
+    parse_afisha_list(html)
